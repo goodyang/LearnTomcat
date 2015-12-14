2fef6011e3d74c1da87ba9ed7074faf7e63afddc
goodyang <972750460@qq.com>
2015-12-14 22:39:51 +0800

    First Version

---
+++ LearnTomcat/src/main/java/com/goodyang/LearnTomcat/App.java
@@ -0,0 +1,13 @@
+package com.goodyang.LearnTomcat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
